2c92cc84eec7a6bb45e0589f2629f1c5ec88ffb5
anaisrmzl <anaisrmzl@gmail.com>
2020-02-26 19:48:10 -0600

    added characters follow behavior

---
+++ Mico Emotion/Assets/Main/Utilities/UI/FollowUI.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Utilities.UI
+{
+    public class FollowUI : MonoBehaviour
+    {
+        #region FIELDS
+
+        [SerializeField] private GameObject target;
+
+        private float offset;
+
+        #endregion
+
+        #region BEHAVIORS
+
+        private void Awake()
+        {
+            offset = Camera.main.ViewportToWorldPoint(Vector2.right).x;
+        }
+
+        private void Update()
+        {
+            transform.position = new Vector3(target.transform.position.x + offset, transform.position.y, transform.position.z);
+        }
+
+        #endregion
+    }
+}
