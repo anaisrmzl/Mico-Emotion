eeb7b0a4bbba99272386c2c4ceed54766e5848ff
anaisrmzl <anaisrmzl@gmail.com>
2020-02-26 20:16:51 -0600

    Splash screen (#12)
    * added splash screen
    
    * deleted unnecessary constant

---
+++ Mico Emotion/Assets/Main/Scripts/Splash/SplashManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using Zenject;
+
+namespace Emotion.Splash
+{
+    public class SplashManager : MonoBehaviour
+    {
+        #region FIELDS
+
+        [Inject] private SplashModule splashModule = null;
+
+        #endregion
+
+        #region BEHAVIORS
+
+        private void Start()
+        {
+            StartSplash();
+        }
+
+        private void StartSplash()
+        {
+            splashModule.StartSplash(GoToFirstScene);
+        }
+
+        private void GoToFirstScene()
+        {
+            SceneManager.LoadScene(SceneNames.Screen1);
+        }
+
+        #endregion
+    }
+}
---
+++ Mico Emotion/Assets/Main/Scripts/Splash/SplashModule.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Emotion.Splash
+{
+    public class SplashModule : MonoBehaviour
+    {
+        #region FIELDS
+
+        [Header("COMPONENTS")]
+        [SerializeField] private GameObject container = null;
+
+        private ISplashScreen splashScreen = null;
+        private UnityAction onSplashEnded = null;
+
+        #endregion
+
+        #region BEHAVIORS
+
+        private void Awake()
+        {
+            splashScreen = GetComponentInChildren<ISplashScreen>();
+        }
+
+        public void StartSplash(UnityAction onSplashEnd)
+        {
+            this.onSplashEnded = onSplashEnd;
+            splashScreen.Appear();
+        }
+
+        public void EndSplash()
+        {
+            container.SetActive(false);
+            onSplashEnded?.Invoke();
+        }
+
+        #endregion
+    }
+}
