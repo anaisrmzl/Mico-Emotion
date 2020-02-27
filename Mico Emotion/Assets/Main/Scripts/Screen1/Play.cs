98a5e085f5d2843b1160efc2db1dfcf271e9deb2
anaisrmzl <anaisrmzl@gmail.com>
2020-02-26 20:48:01 -0600

    Created mock up animation for intro

---
+++ Mico Emotion/Assets/Main/Scripts/Screen1/Play.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Emotion.Screen1
+{
+    public class Play : MonoBehaviour
+    {
+        #region FIELDS
+
+        [SerializeField] private AnimationClip planetIntroAnimation;
+
+        #endregion
+
+        #region BEHAVIORS
+
+        #endregion
+    }
+}
