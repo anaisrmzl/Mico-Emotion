191bbb98d605a1defaeb6eadc381c15aa6de39e8
anaisrmzl <anaisrmzl@gmail.com>
2020-02-29 19:45:15 -0600

    Added play and return buttons behaviors

--- Mico Emotion/Assets/Main/Scripts/Screen1/Play.cs
+++ Mico Emotion/Assets/Main/Scripts/Screen1/Play.cs
@@ -1,13 +1,17 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Playables;
+using System.Collections;
 
 namespace Emotion.Screen1
 {
+    [RequireComponent(typeof(Button))]
     public class Play : MonoBehaviour
     {
         #region FIELDS
 
         [SerializeField] private AnimationClip pressAnimation;
+        [SerializeField] private PlayableDirector timelineSequence;
 
         private Button playButton;
         private Animator animator;
@@ -20,12 +24,23 @@ namespace Emotion.Screen1
         {
             animator = GetComponent<Animator>();
             playButton = GetComponent<Button>();
-            playButton.onClick.AddListener(PlayGame);
+            playButton.onClick.AddListener(PressButton);
         }
 
-        private void PlayGame()
+        private void PressButton()
         {
             animator.Play(pressAnimation.name);
+            playButton.interactable = false;
+            StartCoroutine(PlaySequence());
+        }
+
+        private IEnumerator PlaySequence()
+        {
+            yield return new WaitForSeconds(pressAnimation.length);
+            timelineSequence.Stop();
+            timelineSequence.Play();
+            yield return new WaitForSeconds((float)timelineSequence.duration);
+            playButton.interactable = true;
         }
 
         #endregion
