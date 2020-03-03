9e30172b68fa9e559c1e4dcfd0f9d3e21897cc10
anaisrmzl <anaisrmzl@gmail.com>
2020-03-02 19:09:35 -0600

    Added transition between scenes

---
+++ Mico Emotion/Assets/Main/Utilities/FadeSceneChanger.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Utilities.Scenes
+{
+    public class FadeSceneChanger : MonoBehaviour
+    {
+        #region FIELDS
+
+        private const string PathToPrefab = "Scene/FadeSceneChanger";
+
+        [SerializeField] private Animator animator = null;
+        [SerializeField] private AnimationClip transitionAnimation = null;
+
+        #endregion
+
+        #region BEHAVIOURS
+
+        public static void ChangeScene(string sceneName)
+        {
+            FadeSceneChanger fadeScenePrefab = Resources.Load<FadeSceneChanger>(PathToPrefab);
+            Instantiate(fadeScenePrefab, Vector3.zero, Quaternion.identity).FadeIntoScene(sceneName);
+        }
+
+        private void FadeIntoScene(string sceneName)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            StartCoroutine(PlayTransitionAnimation(sceneName));
+        }
+
+        private IEnumerator PlayTransitionAnimation(string sceneName)
+        {
+            animator.Play(transitionAnimation.name);
+            yield return new WaitForSeconds(transitionAnimation.length);
+            SceneManager.LoadScene(sceneName);
+            yield return new WaitForSeconds(transitionAnimation.length);
+            Destroy(this.gameObject);
+        }
+
+        #endregion
+    }
+}
