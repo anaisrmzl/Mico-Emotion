71d4285e356c4f9095a33f2a58763ed5555cb14b
anaisrmzl <anaisrmzl@gmail.com>
2020-03-07 23:36:51 -0600

    piling stones behaviors

---
+++ Mico Emotion/Assets/Main/Scripts/Screen3/DragStone.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+using Utilities.Gestures;
+using DG.Tweening;
+
+namespace Emotion.Screen3
+{
+    public class DragStone : DragObject
+    {
+        #region FIELDS
+
+        private const float MinDistance = 1.0f;
+        private const float TweenDuration = 0.2f;
+        private const string FloorTag = "Floor";
+        private const string FixedRockTag = "FixedRock";
+
+        private StonesManager stonesManager;
+
+        #endregion
+
+        #region BEHAVIORS
+
+        public override void Update()
+        {
+            if (!DragAllowed)
+                return;
+
+            base.Update();
+            Vector2 lowPoint = new Vector2(transform.position.x, objectCollider.bounds.min.y);
+            Vector2 towerHighestPoint = new Vector2(stonesManager.BaseX, stonesManager.BoundY);
+            float distance = Vector2.Distance(lowPoint, towerHighestPoint);
+            if (distance > MinDistance)
+                return;
+
+            Vector2 finalPosition = new Vector2(stonesManager.BaseX, towerHighestPoint.y + (objectCollider.bounds.center.y - objectCollider.bounds.min.y));
+            DragAllowed = false;
+            rigidBody.velocity = Vector2.zero;
+            rigidBody.gravityScale = 0;
+            transform.DOMove(finalPosition, TweenDuration).OnComplete(SetStatic);
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.transform.tag == StonesManager.PileTag && transform.parent == null)
+            {
+                rigidBody.bodyType = RigidbodyType2D.Static;
+                stonesManager.AddToThePile(gameObject);
+            }
+
+            if (other.transform.tag == FloorTag)
+                rigidBody.gravityScale = 1;
+        }
+
+        public void Initialize(StonesManager manager)
+        {
+            stonesManager = manager;
+        }
+
+        private void SetStatic()
+        {
+            rigidBody.gravityScale = 1;
+            gameObject.layer = LayerMask.NameToLayer(FixedRockTag);
+        }
+
+        #endregion
+    }
+}
---
+++ Mico Emotion/Assets/Main/Scripts/Screen3/StonesManager.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+using Utilities.Zenject;
+
+namespace Emotion.Screen3
+{
+    public class StonesManager : MonoBehaviour
+    {
+        #region FIELDS
+
+        public const string PileTag = "Pile";
+        private const int MaxStones = 6;
+
+        [SerializeField] private DragStone[] stones;
+        [SerializeField] private Vector2 spawningPoint;
+
+        #endregion
+
+        #region PROPERTIES
+
+        public float BoundY { get; private set; }
+        public float BaseX { get => transform.GetChild(0).transform.position.x; }
+
+        #endregion
+
+        #region BEHAVIORS
+
+        private void Awake()
+        {
+            SetBoundY();
+        }
+
+        public void AddToThePile(GameObject newStone)
+        {
+            newStone.transform.SetParent(transform);
+            newStone.tag = PileTag;
+            newStone.GetComponent<SpriteRenderer>().sortingOrder = 0;
+            SetBoundY();
+        }
+
+        private void SetBoundY()
+        {
+            InstantiateStone();
+            Collider2D objectCollider = transform.GetChild(transform.childCount - 1).GetComponent<Collider2D>();
+            BoundY = objectCollider.bounds.max.y;
+        }
+
+        private void InstantiateStone()
+        {
+            if (transform.childCount >= MaxStones)
+                return;
+
+            Vector3 position = new Vector3(Random.Range(-spawningPoint.x, spawningPoint.x), spawningPoint.y, 0.0f);
+            DragStone chosen = stones[Random.Range(0, stones.Length)];
+            DragStone stone = ZenjectUtilities.Instantiate<DragStone>(chosen, position, chosen.transform.rotation, null);
+            stone.transform.localScale = chosen.transform.localScale;
+            stone.Initialize(this);
+        }
+
+        #endregion
+    }
+}
