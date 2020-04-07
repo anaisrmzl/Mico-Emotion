4442a4a82d1ad1678bb7640c3d28a5afd94790cb
anaisrmzl <anaisrmzl@gmail.com>
2020-04-06 22:48:55 -0500

    added info parents scene and planet's confetti (#52)

---
+++ Mico Emotion/Assets/Main/Utilities/URL/OpenURL.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Utilities.URL
+{
+    [RequireComponent(typeof(Button))]
+    public class OpenURL : MonoBehaviour
+    {
+        #region FIELDS
+
+        [SerializeField] private string URL;
+
+        #endregion
+
+        #region BEHAVIORS
+
+        private void Awake()
+        {
+            GetComponent<Button>().onClick.AddListener(GoToURL);
+        }
+
+        private void GoToURL()
+        {
+            Application.OpenURL(URL);
+        }
+
+        #endregion
+    }
+}
