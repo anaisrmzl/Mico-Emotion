7ab5f3f0a52a1ff4355315e555ed79564cbd2cab
anaisrmzl <anaisrmzl@gmail.com>
2020-04-12 22:01:25 -0500

    added cards

---
+++ Mico Emotion/Assets/Main/Scripts/ActivityCards/ActivityCard.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+using Utilities.Sound;
+using Zenject;
+
+namespace Emotion.ActivityCards
+{
+    public class ActivityCard : MonoBehaviour
+    {
+        #region FIELDS
+
+        private const float TimeToAppear = 900.0f;
+        private const float TimeToSeeCard = 10.0f;
+        private const string InTrigger = "In";
+        private const string OutTrigger = "Out";
+
+        [Inject] private SoundManager soundManager;
+
+        [SerializeField] private Sprite[] cards;
+        [SerializeField] private Sprite[] closeButtons;
+        [SerializeField] private Image cardImage;
+        [SerializeField] private Image closeButtonImage;
+        [SerializeField] private Animator cardAnimator;
+        [SerializeField] private Button closeButton;
+
+        private float counter = 0.0f;
+        private bool count = true;
+        private int cardIndex = 0;
+
+        #endregion
+
+        #region BEHAVIORS
+
+        private void Awake()
+        {
+            cardIndex = Random.Range(0, cards.Length);
+            closeButton.onClick.AddListener(CloseCard);
+        }
+
+        private void Update()
+        {
+            if (!count)
+                return;
+
+            counter += Time.deltaTime;
+            if (counter >= TimeToAppear)
+                StartCoroutine(ShowCard());
+        }
+
+        private IEnumerator ShowCard()
+        {
+            count = false;
+            counter = 0.0f;
+            cardIndex++;
+            cardIndex = cardIndex >= cards.Length ? 0 : cardIndex;
+            cardImage.sprite = cards[cardIndex];
+            closeButtonImage.sprite = closeButtons[cardIndex];
+            cardAnimator.SetTrigger(InTrigger);
+            yield return new WaitForSeconds(1.0f);
+            Time.timeScale = 0;
+            soundManager.PauseMusic(true);
+            soundManager.StopEffect();
+            soundManager.StopVoice();
+            yield return new WaitForSeconds(TimeToSeeCard);
+            CloseCard();
+        }
+
+        private void CloseCard()
+        {
+            Time.timeScale = 1;
+            soundManager.PauseMusic(false);
+            StopAllCoroutines();
+            cardAnimator.SetTrigger(OutTrigger);
+            count = true;
+        }
+
+        #endregion
+    }
+}
